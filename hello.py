c7928adb4b6f8da635b716e96c32d6e669fc9887
jormondroyd27 <jimmy.ormondroyd@outlook.com>
2020-02-29 15:46:48 +0000

    added session 1

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello, Jimmy")
+
+#1. Create two variables, one that holds the width and one that holds the height of a rectangle, then, work out and store the variable in a third variable.
+width = 4
+height = 8
+area = width * height 
+print("rectangle of width " + str(width) + " and height " + str(height) + " has an area of " + str(area))
+
+#2 Write the code 
+#2 Write the code that prints the length of the string, 'python'.
+print(len('python'))
